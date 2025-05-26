4a4ce816f9f065c67dad9779ac7148fe8ea86d7f
ghost518 <ghost518@163.com>
2025-05-26 17:16:12 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,18 +1,25 @@
+
 #!/bin/bash
-set -eux
+set -eux
 
-TARGET_REGISTRY=registry.cn-hangzhou.aliyuncs.com
-TARGET_NAMESPACE=ghost518
-IMAGES="langgenius/dify-api:1.3.0 langgenius/dify-web:1.3.0 langgenius/dify-plugin-daemon:0.0.8-local"
+TARGET_REGISTRY="registry.cn-hangzhou.aliyuncs.com"
+TARGET_NAMESPACE="ghost518"
+IMAGES=(
+  "langgenius/dify-api:1.3.0"
+  "langgenius/dify-web:1.3.0" 
+  "langgenius/dify-plugin-daemon:0.0.8-local"
+)
 
-for image in ${IMAGES};do
-    # 拉取镜像
-    docker pull $image
-    name=`echo ${image} | cut -d '/' -f2`
-    tag=`echo ${name} | cut -d ':' -f2`
-    targetFullName=${TARGET_REGISTRY}/${TARGET_NAMESPACE}/${name}
-    # 打阿里云的tag
-    docker tag ${image} ${targetFullName}
-    # 推送到阿里云
-    docker push ${targetFullName}
+for image in "${IMAGES[@]}"; do
+    # 拉取镜像
+    docker pull "$image"
+    name=$(echo "${image}" | cut -d '/' -f2)
+    pure_name=$(echo "${name}" | cut -d ':' -f1)
+    tag=$(echo "${name}" | cut -d ':' -f2)
+    targetFullName="${TARGET_REGISTRY}/${TARGET_NAMESPACE}/${pure_name}:${tag}"
+    
+    # 打阿里云的tag
+    docker tag "${image}" "${targetFullName}"
+    # 推送到阿里云
+    docker push "${targetFullName}"
 done
